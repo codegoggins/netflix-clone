ab546c19b80475b951d2e9b93f8c87181dbb110f
Nilay Singh <snilay011@gmail.com>
2023-01-27 19:23:59 +0530

    Routing Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import './App.css';
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Account from "./pages/Account";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+const Layout = () => {
+  return (
+    <div className="app">
+         <Navbar/>
+         <Outlet/>
+         <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Layout/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/signin',
+        element:<SignIn/>
+      },
+      {
+        path:'/signup',
+        element:<SignUp/>
+      },
+      {
+        path:'/account',
+        element:<Account/>
+      },
+    ]
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-       <h1 className='text-3xl'>Hello</h1>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
