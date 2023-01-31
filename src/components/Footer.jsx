04bf6a1dcf148a52b58af0a2e9faa5de37fbf859
Nilay Singh <snilay011@gmail.com>
2023-01-31 13:31:06 +0530

    Account Page Completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='h-[80px] border-t-2 border-[#303030]'>
+    <div className='h-[80px] border-t-2 border-[#303030] mt-20'>
       <div className='text-[#303030] flex gap-2 items-center justify-center p-6'>
         <p>
         © 2022 Netflix Clone All Rights Reserved by
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <div>
+    {/* IMAGE */}
+       <div className='h-[400px]'>
+          <div className='absolute w-full h-[400px] top-0 left-0 bg-black/60'></div>
+          <img className='h-full w-full object-cover' 
+           src="https://assets.nflxext.com/ffe/siteui/vlv3/e451379a-dd0a-4657-b530-4ca4c0cb2aee/430b26cf-b6e1-473e-a55d-0abc03631481/IN-en-20230123-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="" />
+       </div>
+    {/* SHOWS */}
+        <div className='p-4 pb-8'>
+            <h1 className='text-xl font-bold'>My Shows</h1>
+        </div>
+    </div>
   )
 }
 
